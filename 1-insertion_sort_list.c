d5e06bb13bb5dd8b3e968a602bbafc4716a51f56
victorcreativee <tuyizerevictor@gmail.com>
2024-10-28 08:58:28 -0700

    insertion array

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,47 @@
+#include "sort.h"
+/**
+ * insertion_sort_list - sort a list
+ * @list: UnSorted Linked list
+ */
+
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *node;
+
+	if (list == NULL || (*list)->next == NULL)
+		return;
+	node = (*list)->next;
+	while (node)
+	{
+		while ((node->prev) && (node->prev->n > node->n))
+		{
+			node = swap_node(node, list);
+			print_list(*list);
+		}
+		node = node->next;
+	}
+}
+/**
+ *swap_node - swap a node helper
+ *@node: node
+ *@list: node list
+ *Return: return pointer of current node
+ */
+
+listint_t *swap_node(listint_t *node, listint_t **list)
+{
+	listint_t *back = node->prev, *current = node;
+	/*NULL, 19, 48, 9, 71, 13, NULL*/
+
+	back->next = current->next;
+	if (current->next)
+		current->next->prev = back;
+	current->next = back;
+	current->prev = back->prev;
+	back->prev = current;
+	if (current->prev)
+		current->prev->next = current;
+	else
+		*list = current;
+	return (current);
+}
